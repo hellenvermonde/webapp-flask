cc35ea90a5e4c495ac0948ac0575943b2168bda9
Hellen Vermonde <hellenvermonde@gmail.com>
2022-09-24 19:17:51 -0300

    flas app

---
+++ jogoteca.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def ola():
+    return render_template('lista.html')
+
+# trecho da app
+app.run(host='0.0.0.0', port=8080)
